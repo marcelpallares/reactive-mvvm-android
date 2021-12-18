abf8fc58795621e6ca01c79e5f9a8a14e8855c13
marcel.pallares <marcel.pallares@zego.com>
2021-12-18 14:07:20 +0000

    Refactor `MainActivity` to use view bindings

--- app/src/main/java/com/mpgcode/reactivemvvm/MainActivity.kt
+++ app/src/main/java/com/mpgcode/reactivemvvm/MainActivity.kt
@@ -2,10 +2,14 @@ package com.mpgcode.reactivemvvm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mpgcode.reactivemvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
