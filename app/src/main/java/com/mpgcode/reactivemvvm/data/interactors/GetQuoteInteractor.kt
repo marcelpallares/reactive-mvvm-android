38bec4e58f7b2920b2ce703eaa5033ccc742d1a0
marcel.pallares <marcel.pallares@zego.com>
2021-12-18 16:08:34 +0000

    Refactor `GetQuoteInteractor`

--- app/src/main/java/com/mpgcode/reactivemvvm/data/interactors/GetQuoteInteractor.kt
+++ app/src/main/java/com/mpgcode/reactivemvvm/data/interactors/GetQuoteInteractor.kt
@@ -3,28 +3,16 @@ package com.mpgcode.reactivemvvm.data.interactors
 import android.content.Context
 import com.mpgcode.reactivemvvm.data.models.Quote
 import com.mpgcode.reactivemvvm.data.repositories.QuotesRepository
-import kotlinx.coroutines.CoroutineDispatcher
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.flow.shareIn
+import kotlinx.coroutines.*
 
 class GetQuoteInteractor(
     context: Context,
-    ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
-    externalScope: CoroutineScope = CoroutineScope(ioDispatcher),
+    private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO,
     private val repository: QuotesRepository = QuotesRepository(context),
     private val loadingIntervalMs: Long = 3000
 ) {
-    val randomQuoteFlow: Flow<Quote> = flow {
+    suspend fun getQuote(): Quote = withContext(ioDispatcher) {
         delay(loadingIntervalMs) // Wait to emulate a real network connection delay
-        emit(repository.getQuotesFromAssets().random())
-    }.shareIn(
-        scope = externalScope,
-        replay = 1,
-        started = SharingStarted.WhileSubscribed(5000)
-    )
+        repository.getQuotesFromAssets().random()
+    }
 }
