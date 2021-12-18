3fb9bf5e7cf552252b42f2f7f35e8186ddbf18ae
marcel.pallares <marcel.pallares@zego.com>
2021-12-18 15:41:28 +0000

    Add view extensions

---
+++ app/src/main/java/com/mpgcode/reactivemvvm/view/utils/ViewExtensions.kt
@@ -0,0 +1,15 @@
+package com.mpgcode.reactivemvvm.view.utils
+
+import android.app.Activity
+import android.content.Intent
+
+fun Activity.shareText(text: String) {
+    val sendIntent: Intent = Intent().apply {
+        action = Intent.ACTION_SEND
+        putExtra(Intent.EXTRA_TEXT, text)
+        type = "text/plain"
+    }
+
+    val shareIntent = Intent.createChooser(sendIntent, null)
+    startActivity(shareIntent)
+}
